0d90368b730a476df8f00ca381fae6f87d2b06dc
pradeepa23 <pradeepa23.m@gmail.com>
2022-05-08 16:54:38 -0400

    first commit

---
+++ Practice1.py
@@ -0,0 +1,18 @@
+a=[10,20,30]
+for i in a:
+    print("Print i",i)
+else:
+    print("Else block")  
+
+    li=[5,8,10,20,30]
+s=0
+for i in li:
+    s=s+i
+    print(s)
+
+    def first_name(data);
+    for i in data:
+        print(i.split()[0])
+def last_name(data):
+    for i in data:
+        print(i.split()[1])
---
+++ practice.py
@@ -0,0 +1,6 @@
+def first_name(data);
+    for i in data:
+        print(i.split()[0])
+def last_name(data):
+    for i in data:
+        print(i.split()[1])
